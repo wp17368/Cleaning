6e37449341a68545dcb80b4e550ad60f879c29d3
wasp <margi4@op.pl>
2024-03-01 21:37:14 +0100

    add navbar

---
+++ Darpres/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, NavComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Darpres';
+}
